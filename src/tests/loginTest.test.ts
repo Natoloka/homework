d3db57f8046fed50ea8c77b4a85eb466baa0afbc
Natoloka <natoloka@ukr.net>
2024-06-10 22:44:29 +0300

    removed commented code

--- src/tests/loginTest.test.ts
+++ src/tests/loginTest.test.ts
@@ -6,44 +6,7 @@ import { LoginPage } from '../pages/LoginPage';
 test('test', async ({ page }) => {
   const loginPage = new LoginPage(page);
   await loginPage.navigate(process.env.BASE_URL!);
- 
-  // await page.getByRole('button', { name: 'Login' }).click();
-  // await expect(page.getByRole('button', { name: 'Login' })).toBeVisible();
-  
   await loginPage.loginWith(process.env.USERNAME!, process.env.PASSWORD!);
-// await expect(loginPage).toBeVisible();
-  // await page.getByPlaceholder('Username').click();
-  // await page.getByPlaceholder('Username').fill('admin');
-  // await page.getByRole('button', { name: 'Login' }).click();
-  // await page.getByPlaceholder('Password').click();
-  // await page.getByPlaceholder('Password').fill('admin');
-  // await page.getByRole('button', { name: 'Login' }).click();
-  // await page.getByPlaceholder('Username').click();
-  // await page.getByPlaceholder('Username').fill('admin1');
-  // await page.getByRole('button', { name: 'Login' }).click();
-  // await page.getByPlaceholder('Password').click();
-  // await page.getByPlaceholder('Password').fill(' ');
-  // await page.getByRole('button', { name: 'Login' }).click();
-  // await page.getByPlaceholder('Username').click();
-  // await page.getByPlaceholder('Username').fill(' ');
-  // await page.getByRole('button', { name: 'Login' }).click();
-  // await page.getByPlaceholder('Username').click();
-  // await page.getByPlaceholder('Username').fill('1');
-  // await page.getByPlaceholder('Password').click();
-  // await page.getByPlaceholder('Password').fill('1');
-  // await page.getByRole('button', { name: 'Login' }).click();
-  // await page.getByPlaceholder('Username').click();
-  // await page.getByPlaceholder('Username').fill('!');
-  // await page.getByPlaceholder('Password').click();
-  // await page.getByPlaceholder('Password').fill('1');
-  // await page.getByRole('button', { name: 'Login' }).click();
-  // await page.getByPlaceholder('Username').click();
-  // await page.getByPlaceholder('Username').fill('select * from users');
-  // await page.getByRole('button', { name: 'Login' }).click();
-  // await page.getByPlaceholder('Password').click();
-  // await page.getByPlaceholder('Password').fill('1');
-  // await page.getByRole('button', { name: 'Login' }).click();
-  // await page.getByPlaceholder('Password').click();
 });
 
 test('test2', async ({ page }) => {
